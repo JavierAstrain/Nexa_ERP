478a566674cd800d0b668c30beec9042bc77d187
JavierAstrain <j.astrain.import@gmail.com>
2025-09-15 15:43:47 -0300

    Update prisma.ts

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,2 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+// Import seguro para evitar errores de types cuando aún no se ha corrido `prisma generate`
+import prismaPkg from '@prisma/client';
+const { PrismaClient } = prismaPkg as any;
+
 export const prisma = new PrismaClient();
